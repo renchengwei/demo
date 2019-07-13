800b1b337bc667f093efc4b1c408ffb3c8d1b744
任成威 <627127819@qq.com>
2019-07-13 23:11:13 +0800

    添加common mybatis

--- src/main/java/com/example/demo/mapper/UserMapper.java
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -2,9 +2,11 @@ package com.example.demo.mapper;
 
 import com.example.demo.entity.User;
 import org.springframework.stereotype.Repository;
+import tk.mybatis.mapper.common.Mapper;
 
 @Repository
-public interface UserMapper {
+public interface UserMapper extends Mapper<User> {
 
     User getUser(String userName);
+
 }
